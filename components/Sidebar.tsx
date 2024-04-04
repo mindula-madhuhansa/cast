7b94ab1146127b1dc85c6d01e05dcf405c80b373
mindula-madhuhansa <mimmindula@gmail.com>
2024-04-04 22:39:53 +0530

    add mobile nav and sidebar components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 const Sidebar = () => {
   const pathname = usePathname();
